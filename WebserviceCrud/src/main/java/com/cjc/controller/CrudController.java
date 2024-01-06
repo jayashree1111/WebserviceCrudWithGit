8f20e1be156cee87b05af2849a07ced193572c58
TapeshwarSherekar <jsherekar17@gmail.com>
2024-01-06 16:19:07 +0530

    controller

--- WebserviceCrud/src/main/java/com/cjc/controller/CrudController.java
+++ WebserviceCrud/src/main/java/com/cjc/controller/CrudController.java
@@ -1,10 +1,29 @@
 package com.cjc.controller;
 
+import java.util.List;
+
 import org.hibernate.dialect.MySQLDialect;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.model.Student;
 
 @RestController
 public class CrudController {
+	@PostMapping("/save")
+	public String saveData(@RequestBody Student s)
+	{
+		return null;
+		
+	}
+	@GetMapping("/get")
+	public List<Student> getData()
+	{
+		return null;
+		
+	}
 	
 	
 	
--- WebserviceCrud/src/main/java/com/cjc/model/Student.java
+++ WebserviceCrud/src/main/java/com/cjc/model/Student.java
@@ -1,8 +1,12 @@
 package com.cjc.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+
+@Entity
 public class Student 
-{
+{   @Id
 	private int id;
 	private String name;
 	private String address;
