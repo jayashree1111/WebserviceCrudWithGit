37806409272c781ae3e27638ce46a5587efad60a
TapeshwarSherekar <jsherekar17@gmail.com>
2024-01-06 15:34:56 +0530

    by jayashree only properties file

---
+++ WebserviceCrud/src/main/java/com/cjc/WebserviceCrudApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceCrudApplication.class, args);
+	}
+
+}
---
+++ WebserviceCrud/src/main/java/com/cjc/controller/CrudController.java
@@ -0,0 +1,13 @@
+package com.cjc.controller;
+
+import org.hibernate.dialect.MySQLDialect;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CrudController {
+	
+	
+	
+	
+
+}
